bbe31e0d6c14ea26c5fa5437b9a85221f4f0423a
brent <hronik1@illinois.edu>
2012-08-27 20:18:02 -0500

    mainactivity predatabase connection

--- src/com/example/fitsbypact/MainActivity.java
+++ src/com/example/fitsbypact/MainActivity.java
@@ -1,20 +1,121 @@
 package com.example.fitsbypact;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private final static String TAG = "MainActivity";
+	
+	private Button buttonLogin;
+	private Button buttonRegister;
+	
+	private EditText emailET;
+	private EditText passwordET;
+	
+	/**
+	 * called when activity is created
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+        
+        initializeButtons();
+        initializeEditTexts();
+        
+    }
+    
+    /**
+     * called when activity is restarted
+     */
+    @Override
+    public void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart");
     }
 
+    /**
+     * called when activity is starting
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        Log.i(TAG, "onStart");
+    }
+    
+    /**
+     * called when activity resumes
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        Log.i(TAG, "onResume");
+    }
+    
+    /**
+     * called when activity is paused
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+       
+        Log.i(TAG, "onPause" + (isFinishing() ? " Finishing" : " Not Finishing"));
+    }
+    
+    /**
+     * called when activity is destroyed
+     */
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	
+    	Log.i(TAG, "onDestroy");
+    	
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    /**
+     * helper function which initializes the buttons
+     */
+    private void initializeButtons() {
+    	buttonLogin = (Button)findViewById(R.id.login_button);
+    	buttonLogin.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			//TODO login user
+    		}
+    	});
+    	
+    	buttonRegister = (Button)findViewById(R.id.register_button);
+    	buttonRegister.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			//TODO register user
+    		}
+    	});
+    }	
+    	
+    /** 
+     * helper function which initializes the EditTexts
+     */
+    private void initializeEditTexts() {
+    	emailET = (EditText)findViewById(R.id.login_email_id);
+    	passwordET = (EditText)findViewById(R.id.login_password_id);
+    }
+
 }
