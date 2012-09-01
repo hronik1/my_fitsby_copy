576f0f1ccb20ae97ebe89de3aa5fca5863df9ebe
brent <hronik1@illinois.edu>
2012-08-31 20:59:39 -0500

    landing page and registration page

--- src/registration/RegisterClientSideValidation.java
+++ src/registration/RegisterClientSideValidation.java
@@ -4,23 +4,56 @@ public class RegisterClientSideValidation {
 
 	private static final int minLength = 6;
 	
+
+
 	/**
-	 * brief client-side validation
-	 * @param email
-	 * @param password
-	 * @return empty string if valid, error otherwise
+	 * ensures firstName and lastName are not empty
+	 * @param firstName
+	 * @param lastName
+	 * @return an empty string if success, an error message
 	 */
-	public static String validate(String email, String password) {
+	public static String validateName(String firstName, String lastName) {
+		String ret = "";
+		
+		if (firstName == null || firstName.length() == 0) {
+			ret += "Must fill out first name\n";
+			
+		}
+		if (lastName == null || lastName.length() == 0) {
+			ret += "Must fill out last name\n";
+		}
+		
+		return ret;
+	}
+	
+	/**
+	 * validates email
+	 * @param email
+	 * @return empty string if valid, error message otherwise
+	 */
+	public static String validateEmail(String email) {
 		String ret = "";
 		
 		if (email == null || email.length() < minLength) {
-			ret += "email too short ";
+			ret += "email too short\n";
 			//TODO additional email verification
 		}
 		
+		return ret;
+	}
+	
+	/**
+	 * validates password
+	 * @param password
+	 * @return empty string if fine, error message otherwise
+	 */
+	public static String validatePassword(String password) {
+		String ret = "";
+		
 		if (password == null || password.length() < minLength) {
-			ret += "password too short";
+			ret += "password too short\n";
 		}
+		//TODO additional password verification
 		
 		return ret;
 	}
