bf58cfacd9d0aba43d4df4fc44282fbc32031a51
brent <hronik1@illinois.edu>
2012-08-31 21:03:21 -0500

    landing and register layout files

---
+++ src/com/example/fitsbypact/LandingActivity.java
@@ -0,0 +1,126 @@
+package com.example.fitsbypact;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LandingActivity extends Activity {
+
+	private final static String TAG = "LandingActivity";
+	
+	private Button buttonLogin;
+	private Button buttonStart;
+	
+	/**
+	 * Called when activity is created
+	 */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        Log.i(TAG, "onCreate");
+        
+        initializeButtons();
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu. You should place your menu items in to menu.
+     * This is only called once, the first time the options menu is displayed
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_landing, menu);
+        
+        Log.i(TAG, "onCreateOptionsMenu");
+        return true;
+    }
+    
+    /**
+     * called when activity is restarted
+     */
+    @Override
+    public void onRestart() {
+        super.onRestart();
+
+        Log.i(TAG, "onRestart");
+    }
+
+    /**
+     * called when activity is starting
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        Log.i(TAG, "onStart");
+    }
+    
+    /**
+     * called when activity resumes
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        Log.i(TAG, "onResume");
+    }
+    
+    /**
+     * called when activity is paused
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+       
+        Log.i(TAG, "onPause" + (isFinishing() ? " Finishing" : " Not Finishing"));
+    }
+    
+    /**
+     * called when activity is destroyed
+     */
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	
+    	Log.i(TAG, "onDestroy");
+    	
+    }
+
+    /**
+     * initializes the login and start buttons 
+     */
+    private void initializeButtons() {
+    	buttonLogin = (Button)findViewById(R.id.landing_button_login);
+    	buttonLogin.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			goToLoginPage();
+    		}
+    	});
+    	
+    	buttonStart = (Button)findViewById(R.id.landing_button_start);
+    	buttonStart.setOnClickListener(new OnClickListener() {
+    		public void onClick(View v) {
+    			goToRegisterPage();
+    		}
+    	});
+    }
+    
+    /**
+     * Changes to the LoginActivity
+     */
+    private void goToLoginPage() {
+    	//TODO switch to login activity
+    }
+    
+    /**
+     * Changes to the Registration Activity
+     */
+    private void goToRegisterPage() {
+    	//TODO switch to register activity 
+    }
+    
+}
