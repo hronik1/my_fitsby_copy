9aea69cf25bf515cc800503f4ee5d56aed908b47
Kevin Yun <yun.kevin.s@gmail.com>
2012-09-25 23:23:45 -0500

    remove title bar and notification bar in SplashActivity.java (to clean up splash page)

--- src/com/example/fitsbypact/SplashActivity.java
+++ src/com/example/fitsbypact/SplashActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,6 +16,14 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+      //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+      //Remove notification bar
+      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        
         setContentView(R.layout.activity_splash);
     
     TimerTask task = new TimerTask()
@@ -29,4 +40,6 @@ public class SplashActivity extends Activity {
 	Timer timer = new Timer();
 	timer.schedule(task, SplashDelay);
 	}
+    
+    
 }
