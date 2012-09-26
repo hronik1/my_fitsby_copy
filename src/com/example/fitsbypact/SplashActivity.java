d22770386d581c7e826664e0e14f88dc493fe069
Kevin Yun <yun.kevin.s@gmail.com>
2012-09-25 23:29:39 -0500

    fixin splash duration

--- src/com/example/fitsbypact/SplashActivity.java
+++ src/com/example/fitsbypact/SplashActivity.java
@@ -3,7 +3,6 @@ package com.example.fitsbypact;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
 
