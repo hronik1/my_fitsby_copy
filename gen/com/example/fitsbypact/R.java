031a086b1dcfd8bd44120eb32108dc11761a639b
Kevin Yun <yun.kevin.s@gmail.com>
2012-10-14 18:08:37 -0500

    made new r.java and change a string

--- gen/com/example/fitsbypact/R.java
+++ gen/com/example/fitsbypact/R.java
@@ -32,19 +32,21 @@ public final class R {
         public static final int faq_button_background=0x7f020004;
         public static final int faq_button_text=0x7f020005;
         public static final int game=0x7f020006;
-        public static final int ic_action_search=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int ic_launcher_textlogo=0x7f020009;
-        public static final int landing_getstarted_button=0x7f02000a;
-        public static final int landing_login_button=0x7f02000b;
-        public static final int logo_text=0x7f02000c;
-        public static final int me=0x7f02000d;
-        public static final int newsfeed=0x7f02000e;
-        public static final int newsfeed_submit_button=0x7f02000f;
-        public static final int post_status_here_box=0x7f020010;
-        public static final int public_leagues_top_words=0x7f020011;
-        public static final int wager_box=0x7f020012;
-        public static final int white_textbox=0x7f020013;
+        public static final int green_check_mark=0x7f020007;
+        public static final int ic_action_search=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int ic_launcher_textlogo=0x7f02000a;
+        public static final int landing_getstarted_button=0x7f02000b;
+        public static final int landing_login_button=0x7f02000c;
+        public static final int logo_text=0x7f02000d;
+        public static final int me=0x7f02000e;
+        public static final int newsfeed=0x7f02000f;
+        public static final int newsfeed_submit_button=0x7f020010;
+        public static final int post_status_here_box=0x7f020011;
+        public static final int public_leagues_top_words=0x7f020012;
+        public static final int red_x_mark=0x7f020013;
+        public static final int wager_box=0x7f020014;
+        public static final int white_textbox=0x7f020015;
     }
     public static final class id {
         public static final int activity_league_join_directions=0x7f080049;
