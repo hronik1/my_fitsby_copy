73a32d6cdd2ad403a78169e3ea8b1b3bbf412d3a
brent <hronik1@illinois.edu>
2012-12-03 21:56:37 -0600

    remember me functionality implemented

---
+++ src/constants/RememberMeConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class RememberMeConstants {
+	public static final String PREFS_NAME = "MyPrefsFile";
+	public static final String PREF_EMAIL = "email";
+	public static final String PREF_PASSWORD = "password";
+}
