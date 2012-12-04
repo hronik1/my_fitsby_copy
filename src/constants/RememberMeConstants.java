e585efc6872a151cb6cb2d4de77fed21aefcff0b
brent <hronik1@illinois.edu>
2012-12-03 21:30:22 -0600

    remove remember me constants from master to work on rememberme branch

--- src/constants/RememberMeConstants.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class RememberMeConstants {
-	public static final String PREFS_NAME = "MyPrefsFile";
-	public static final String PREF_USERNAME = "username";
-	public static final String PREF_PASSWORD = "password";
-}
