226688ccbcc9de5c0b6493156495f6dd3bf07f67
brent <hronik1@illinois.edu>
2012-12-03 21:18:25 -0600

    initial remember me functionality

---
+++ src/constants/RememberMeConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class RememberMeConstants {
+	public static final String PREFS_NAME = "MyPrefsFile";
+	public static final String PREF_USERNAME = "username";
+	public static final String PREF_PASSWORD = "password";
+}
