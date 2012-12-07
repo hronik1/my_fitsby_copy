daefc5a81468d2544321a9a5067bb88f46b088c1
brent <hronik1@illinois.edu>
2012-12-07 02:24:11 -0600

    changed flurry constant

--- src/constants/FlurryConstants.java
+++ src/constants/FlurryConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class FlurryConstants {
 
-	public final static String key = "SPXCFGBJFSSSYQM6YD2X";
+	public final static String key = "CCNZ35SC558GJNMWZZCZ";
 	//Flurry Keys:
 	// Testing Key: SPXCFGBJFSSSYQM6YD2X
 	// Public Beta Key: CCNZ35SC558GJNMWZZCZ
