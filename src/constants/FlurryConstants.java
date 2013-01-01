f10a59f4bfb5eb3db75a782851ff2275caff812b
Kevin Yun <yun.kevin.s@gmail.com>
2013-01-01 17:01:57 -0600

    change flurry key and edit cred card stuf

--- src/constants/FlurryConstants.java
+++ src/constants/FlurryConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class FlurryConstants {
 
-	public final static String key = "CCNZ35SC558GJNMWZZCZ";
+	public final static String key = "JFB225VMY7TK853HYZF6";
 	//Flurry Keys:
 	// Testing Key: SPXCFGBJFSSSYQM6YD2X
 	// Public Beta Key: CCNZ35SC558GJNMWZZCZ (DEPRECATED)
