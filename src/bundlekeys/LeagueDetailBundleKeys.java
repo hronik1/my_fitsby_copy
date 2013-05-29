0b5557fea6fc9f05cbca4507dbe46ca94d9016ef
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-05-29 08:16:07 -0600

    comment format LeagueDetailBundleKeys

--- src/bundlekeys/LeagueDetailBundleKeys.java
+++ src/bundlekeys/LeagueDetailBundleKeys.java
@@ -1,13 +1,57 @@
 package bundlekeys;
 
+/**
+ * LeagueDetailBundleKeys is the class which contains keys used to in bundles
+ * to be passed between the master and detail leaague activities.
+ * 
+ * @author brenthronk
+ *
+ */
 public class LeagueDetailBundleKeys {
+	
+	/**
+	 * Key for the league id within bundles.
+	 */
 	public final static String KEY_LEAGUE_ID = "league_id";
+	
+	/**
+	 * Key for wagers within bundles.
+	 */
 	public final static String KEY_WAGER = "wager";
+	
+	/**
+	 * Key for pots within bundles.
+	 */
 	public final static String KEY_POT = "pot";
+	
+	/**
+	 * Key for the type within bundles.
+	 */
 	public final static String KEY_TYPE = "type";
+	
+	/**
+	 * Key for the number of players within bundles.
+	 */
 	public final static String KEY_PLAYERS = "players";
+	
+	/**
+	 * Key for the duration within bundles.
+	 */
 	public final static String KEY_DURATION = "duration";
+	
+	/**
+	 * Key for the bitmap within bundles.
+ 	 */
 	public final static String KEY_BITMAP = "bitmap";
+	
+	/**
+	 * Key for the gym name within bundles.
+	 */
 	public final static String KEY_GYM_NAME = "gym_name";
+	
+	/**
+	 * Key for the goal within bundles.
+	 */
 	public final static String KEY_GOAL = "goal";
+	
 }
