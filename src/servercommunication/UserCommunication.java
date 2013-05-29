81808d1d548db0c692e40506f703b46d2a4332c8
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-05-28 21:50:04 -0600

    removed dead code UserCommunication

--- src/servercommunication/UserCommunication.java
+++ src/servercommunication/UserCommunication.java
@@ -2,37 +2,24 @@ package servercommunication;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import dbtables.User;
 
 import responses.StatsResponse;
 import responses.StatusResponse;
 import responses.UserResponse;
+import android.util.Log;
 
 import com.fitsby.R;
 
 import constants.SingletonContext;
-
-import android.content.res.Resources;
-import android.util.Log;
+import dbtables.User;
 
 public class UserCommunication {
 
@@ -53,21 +40,14 @@ public class UserCommunication {
 	public static UserResponse registerUser(String email, String password, String confirmPassword, String firstName, String lastName) {
 		MyHttpClient myHttpClient = new MyHttpClient();
 		JSONObject json = new JSONObject();
-		List<NameValuePair> params = new LinkedList<NameValuePair>();
         try {
 			json.put("email", email);
 			json.put("password", password);
 			json.put("password_confirmation", confirmPassword);
 			json.put("first_name", firstName);
 			json.put("last_name", lastName);
-			params.add(new BasicNameValuePair("email", email));
-			params.add(new BasicNameValuePair("password", password));
-			params.add(new BasicNameValuePair("confirm_password", confirmPassword));
-			params.add(new BasicNameValuePair("first_name", firstName));
-			params.add(new BasicNameValuePair("last_name", lastName));
 	        StringEntity stringEntity = new StringEntity(json.toString());  
-			//nameValuePairs.add(new BasicNameValuePair("creator_id", creatorId + ""));
-			//TODO add something to nameValuePairs
+	        
 			ServerResponse serverResponse = myHttpClient.createPostRequest(MyHttpClient.SERVER_URL + "users.json", stringEntity);
 			if (serverResponse.exception instanceof IOException) {
 				UserResponse response = new UserResponse();
@@ -92,15 +72,11 @@ public class UserCommunication {
 	 */
 	public static UserResponse loginUser(String email, String password) {
 		MyHttpClient myHttpClient = new MyHttpClient();
-		List<NameValuePair> params = new LinkedList<NameValuePair>();
         try {
         	JSONObject json = new JSONObject();
 			json.put("email", email);
 			json.put("password", password);
 			
-			params.add(new BasicNameValuePair("email", email));
-			params.add(new BasicNameValuePair("password", password));
-			//ServerResponse serverResponse = myHttpClient.createGetRequest(MyHttpClient.SERVER_URL + "login.json", params);
 			StringEntity stringEntity = new StringEntity(json.toString()); 
 			ServerResponse serverResponse = myHttpClient.createPostRequest(MyHttpClient.SERVER_URL + "login_android", stringEntity);
 			if (serverResponse.exception instanceof IOException) {
@@ -163,7 +139,6 @@ public class UserCommunication {
 	
 	public static StatusResponse changeEmail(String email, String userId) {
 		MyHttpClient myHttpClient = new MyHttpClient();
-		List<NameValuePair> params = new LinkedList<NameValuePair>();
 		JSONObject json = new JSONObject();
         try {
         	json.put("new_email", email);
