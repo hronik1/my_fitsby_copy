dcdea45e91d901c431b18e390dd50a0edb24e4e8
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-05-29 08:01:58 -0600

    comment format creditcardbundle

--- src/bundlekeys/CreditCardBundleKeys.java
+++ src/bundlekeys/CreditCardBundleKeys.java
@@ -1,7 +1,22 @@
 package bundlekeys;
 
+/**
+ * This class contains keys to be used in bundles passed from 
+ * CreditCardActivity.
+ * 
+ * @author brenthronk
+ *
+ */
 public class CreditCardBundleKeys {
+	
+	/**
+	 * The key for the wager in the bundle.
+	 */
 	public final static String KEY_WAGER = "wager";
+	
+	/**
+	 * The key for the league id in the bundle.
+	 */
 	public final static String KEY_LEAGUE_ID = "league_id";
 
 }
