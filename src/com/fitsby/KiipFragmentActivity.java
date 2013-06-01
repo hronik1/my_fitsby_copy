204bca97c293b9726c51b610ca29c59ce9493c5d
Brent Hronik <brenthronk@Brents-MacBook-Air.local>
2013-06-01 10:26:49 -0600

    javadoc KiipFragmentActivity

--- src/com/fitsby/KiipFragmentActivity.java
+++ src/com/fitsby/KiipFragmentActivity.java
@@ -7,12 +7,31 @@ import me.kiip.sdk.KiipFragmentCompat;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+/**
+ * KiipFragmentActivity is a base class which all future activities will
+ * subclass, directly or indirectly. It exists to abstract away the details
+ * of implementing kiip in all activities.
+ * 
+ * @author brenthronk
+ *
+ */
 public class KiipFragmentActivity extends FragmentActivity {
 
+	/**
+	 * Key for adding/finding the kiip fragment.
+	 */
     private final static String KIIP_TAG = "kiip_fragment_tag";
 
+    /**
+     * Reference to the kiip fragment.
+     */
     private KiipFragmentCompat mKiipFragment;
 
+    /**
+     * Callback for the creation of the fragment, obtain reference to
+     * kiipfragment.
+     * 
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +45,9 @@ public class KiipFragmentActivity extends FragmentActivity {
         }
     }
 
+    /**
+     * Callback for starting of the activity, starts the kiip session.
+     */
     @Override
     protected void onStart() {
         super.onStart();
@@ -42,12 +64,20 @@ public class KiipFragmentActivity extends FragmentActivity {
         });
     }
 
+    /**
+     * Callback for the stopping of the activity, ends the kiip session.
+     */
     @Override
     protected void onStop() {
         super.onStop();
         Kiip.getInstance().endSession(null);
     }
 
+    /**
+     * Shows the poptart.
+     * 
+     * @param poptart	the poptart to be displayed
+     */
     public void onPoptart(Poptart poptart) {
         mKiipFragment.showPoptart(poptart);
     }
